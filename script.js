94a0208e9c156fe03ca7fe3fa94c2f308d9c5daa
Ayomide Oshilaja <137465548+Amy23m@users.noreply.github.com>
2025-01-12 20:44:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to toggle between light and dark mode
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    const button = document.getElementById('theme-toggle');
+    
+    
+}
